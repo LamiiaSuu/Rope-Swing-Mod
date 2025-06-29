a9698458ef6893d69f410dda09a805f621d740ce
Lamia <dumbdashop@gmail.com>
2025-06-29 23:20:08 +0200

    Git Init
    Started working on the mod

---
+++ Rope Swing Mod/Rope Swing Mod/RopeSwing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rope_Swing_Mod
+{
+    public class RopeSwing
+    {
+    }
+}
